43b51b247c9f9da551942832eed147bf0b4ec325
Luna5379 <110072210+Luna5379@users.noreply.github.com>
2024-09-03 13:58:01 +0100

    added scantokens function and advance function

--- lexer.js
+++ lexer.js
@@ -1,3 +1,5 @@
+import { EaselError } from './stdlib.js'
+
 export const TOKENS = {
   LeftParen: 'LeftParen',
   RightParen: 'RightParen',
@@ -39,5 +41,111 @@ export class Token {
 
   toString() {
     return this.value
+  }
+}
+
+export class Lexer {
+  constructor(program) {
+    this.program = program
+    this.tokens = []
+    this.current = 0
+    this.line = 1
+    this.column = 0
+  }
+
+  error(msg) {
+    throw new EaselError(`Error on ${this.line}:${this.column}: ${msg}`)
+  }
+}
+
+class Lexer {
+
+  peek() {
+    if (this.current >= this.program.length) return '\0'
+    return this.program[this.current]
+  }
+
+  scanToken() {
+    const char = this.advance()
+
+    switch (char) {
+      case '(': {
+        return this.tokens.push(
+          new Token(TOKENS.LeftParen, '(', '(', this.line, this.column)
+        )
+      }
+      case ')':{
+        return this.tokens.push(
+          new Token(TOKENS.RightParen, ')', ')', this.line, this.column)
+        )
+      }
+      case '{': {
+        return this.tokens.push(
+          new Token(TOKENS.LeftBrace, '{', '{', this.line, this.column)
+        )
+      }
+      case '}': {
+        return this.tokens.push(
+          new Token(TOKENS.RightBrace, '}', '}', this.line, this.column)
+        )
+      }
+      case '[': {
+        return this.tokens.push(
+          new Token(TOKENS.LeftBracket, '[', '[', this.line, this.column)
+        )
+      }
+      case ']': {
+        return this.tokens.push(
+          newToken(TOKENS.RightBracket, ']', ']', this.line, this.column)
+        )
+      }
+      case '.': {
+        return this.tokens.push(
+          new Token(TOKENS.Period, '.', '.', this.line, this.column)
+        )
+      }
+      case ',': {
+        return this.tokens.push(
+          new Token(TOKENS.Comma, ',', ',', this.line, this.column)
+        )
+      }
+      case ':': {
+        return this.tokens.push(
+          new Token(TOKENS.Colon, ':', ':', this.line, this.column)
+        )
+      }
+      case '+': {
+        return this.tokens.push(
+          new Token(TOKENS.Plus '+', '+', this.line, this.column)
+        )
+      }
+      case '-': {
+        return this.tokens.push(
+          new Token(TOKENS.Minus, '-', '-', this.line, this.column)
+        )
+      }
+      case '*': {
+        return this.tokens.push(
+          new Token(TOKENS.Asterisk, '*', '*'m this.line, this.column)
+        )
+      }
+      case '/': {
+        return this.tokens.push(
+          new Token(TOKENS.Slash, '/', '/', this.line, this.column)
+        )
+      }
+    }
+  }
+
+  scanTokens() {
+    while (this.peek() !== '\0') this.scanToken()
+    this.tokens.push(new Token(TOKENS.EOF, null, null, this.line, this.column))
+    return this.tokens
+  }
+
+  advance() {
+    if (this.current >= this.program.length) return '\0'
+    this.column++
+    return this.program[this.current++]
   }
 }
