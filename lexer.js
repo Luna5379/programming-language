8036f35f55a36509deae4b7f8f815ec6342613f6
Luna5379 <110072210+Luna5379@users.noreply.github.com>
2024-09-03 14:03:19 +0100

    added more functions, switch, match, peek, struggled to find what was causing an error in the code

--- lexer.js
+++ lexer.js
@@ -30,6 +30,23 @@ export const TOKENS = {
   EOF: 'EOF',
 }
 
+export const KEYWORDS = {
+  prepare: 'prepare',
+  as: 'as',
+  brush: 'brush',
+  prep: 'prep',
+  has: 'has',
+  sketch: 'sketch',
+  needs: 'needs',
+  finished: 'finished',
+  loop: 'loop',
+  through: 'through',
+  while: 'while',
+  if: 'if',
+  elif: 'elif',
+  else: 'else'
+}
+
 export class Token {
   constructor(type, value, content, line, column) {
     this.type = type
@@ -58,102 +75,46 @@ export class Lexer {
   }
 }
 
-class Lexer {
+export class Lexer {
+  constructor(program) {
+    this.program = program
+    this.tokens = []
+    this.current = 0
+    this.line = 1
+    this.column = 1
+  }
+
+  error(msg) {
+    throw new EaselError(`Error on ${this.line}:${this.column}: ${msg}`)
+  }
 
   peek() {
     if (this.current >= this.program.length) return '\0'
     return this.program[this.current]
   }
 
+  advance() {
+    if (this.current >= this.program.length) return '\0'
+    this.column++
+    return this.program[this.current++]
+  }
+
+  match(char) {
+    if (this.peek() === char) return this.advance()
+    return false
+  }
+
   scanToken() {
     const char = this.advance()
 
+    const isNumber = char => char >= '0' && char <= '9'
+    const isChar = char =>
+      (char >= 'A' && char <= 'Z') ||
+      (char >= 'a' && char <= 'z') ||
+      char === '_'
+    const isAlphanumeric = char => isNumber(char) || isChar(char)
+
     switch (char) {
-      case '(': {
-        return this.tokens.push(
-          new Token(TOKENS.LeftParen, '(', '(', this.line, this.column)
-        )
-      }
-      case ')':{
-        return this.tokens.push(
-          new Token(TOKENS.RightParen, ')', ')', this.line, this.column)
-        )
-      }
-      case '{': {
-        return this.tokens.push(
-          new Token(TOKENS.LeftBrace, '{', '{', this.line, this.column)
-        )
-      }
-      case '}': {
-        return this.tokens.push(
-          new Token(TOKENS.RightBrace, '}', '}', this.line, this.column)
-        )
-      }
-      case '[': {
-        return this.tokens.push(
-          new Token(TOKENS.LeftBracket, '[', '[', this.line, this.column)
-        )
-      }
-      case ']': {
-        return this.tokens.push(
-          newToken(TOKENS.RightBracket, ']', ']', this.line, this.column)
-        )
-      }
-      case '.': {
-        return this.tokens.push(
-          new Token(TOKENS.Period, '.', '.', this.line, this.column)
-        )
-      }
-      case ',': {
-        return this.tokens.push(
-          new Token(TOKENS.Comma, ',', ',', this.line, this.column)
-        )
-      }
-      case ':': {
-        return this.tokens.push(
-          new Token(TOKENS.Colon, ':', ':', this.line, this.column)
-        )
-      }
-      case '+': {
-        return this.tokens.push(
-          new Token(TOKENS.Plus '+', '+', this.line, this.column)
-        )
-      }
-      case '-': {
-        return this.tokens.push(
-          new Token(TOKENS.Minus, '-', '-', this.line, this.column)
-        )
-      }
-      case '*': {
-        return this.tokens.push(
-          new Token(TOKENS.Asterisk, '*', '*'m this.line, this.column)
-        )
-      }
-      case '/': {
-        return this.tokens.push(
-          new Token(TOKENS.Slash, '/', '/', this.line, this.column)
-        )
-      }
-      case "'":
-      case '"': {
-        let string = []
-        while (this.peek() !== char) {
-          string.push(this.advance())
-          if (this.peek() === '\0')
-            this.error('Unexpected end of file; expected a closing quote')
-        }
-        this.advance()
-        string = string.join('')
-        return this.tokens.push(
-          new Token(TOKENS.String, string, string, this.line, this.column)
-        )
-      }
-      case '|': {
-        if (this.match('|'))
-          return this.tokens.push(
-            new Token(TOKENS.Or, '||', '||', this.line, this.column)
-          )
-      }
       case '>': {
         if (this.match('='))
           return this.tokens.push(
@@ -178,6 +139,42 @@ class Lexer {
             new Token(TOKENS.Equiv, '==', '==', this.line, this.column)
           )
       }
+      case '(': {
+        return this.tokens.push(
+          new Token(TOKENS.LeftParen, '(', '(', this.line, this.column)
+        )
+      }
+      case ')': {
+        return this.tokens.push(
+          new Token(TOKENS.RightParen, ')', ')', this.line, this.column)
+        )
+      }
+      case '{': {
+        return this.tokens.push(
+          new Token(TOKENS.LeftBrace, '{', '{', this.line, this.column)
+        )
+      }
+      case '}': {
+        return this.tokens.push(
+          new Token(TOKENS.RightBrace, '}', '}', this.line, this.column)
+        )
+      }
+      case '[': {
+        return this.tokens.push(
+          new Token(TOKENS.LeftBracket, '[', '[', this.line, this.column)
+        )
+      }
+      case ']': {
+        return this.tokens.push(
+          new Token(TOKENS.RightBracket, ']', ']', this.line, this.column)
+        )
+      }
+      case '|': {
+        if (this.match('|'))
+          return this.tokens.push(
+            new Token(TOKENS.Or, '||', '||', this.line, this.column)
+          )
+      }
       case '&': {
         if (this.match('&'))
           return this.tokens.push(
@@ -193,23 +190,127 @@ class Lexer {
           new Token(TOKENS.Not, '!', '!', this.line, this.column)
         )
       }
+      case '.': {
+        return this.tokens.push(
+          new Token(TOKENS.Period, '.', '.', this.line, this.column)
+        )
+      }
+      case ',': {
+        return this.tokens.push(
+          new Token(TOKENS.Comma, ',', ',', this.line, this.column)
+        )
+      }
+      case ':': {
+        return this.tokens.push(
+          new Token(TOKENS.Colon, ':', ':', this.line, this.column)
+        )
+      }
+      case '+': {
+        return this.tokens.push(
+          new Token(TOKENS.Plus, '+', '+', this.line, this.column)
+        )
+      }
+      case '-': {
+        return this.tokens.push(
+          new Token(TOKENS.Minus, '-', '-', this.line, this.column)
+        )
+      }
+      case '*': {
+        return this.tokens.push(
+          new Token(TOKENS.Asterisk, '*', '*', this.line, this.column)
+        )
+      }
+      case '/': {
+        return this.tokens.push(
+          new Token(TOKENS.Slash, '/', '/', this.line, this.column)
+        )
+      }
+      case '~': {
+        // Comments
+        while (this.peek() !== '\n' && this.peek() !== null) this.advance()
+        return
+      }
+      case ' ':
+      case '\r': {
+        // Ignore whitespace
+        return
+      }
+      case '\n': {
+        // Also ignore, but update line
+        this.line++
+        this.column = 0
+        return
+      }
+      case "'":
+      case '"': {
+        // String
+        let string = []
+        while (this.peek() !== char) {
+          string.push(this.advance())
+          if (this.peek() === null)
+            // String wasn't closed
+            this.error('Unexpected end of file; expected a closing quote')
+        }
+        this.advance() // Skip closing quote
+        string = string.join('')
+        return this.tokens.push(
+          new Token(TOKENS.String, string, string, this.line, this.column)
+        )
+      }
+      default: {
+        if (isNumber(char)) {
+          let number = [char]
+          while (
+            isNumber(this.peek()) ||
+            (this.peek() === '.' && !number.includes('.'))
+          )
+            number.push(this.advance())
+          number = number.join('')
+          return this.tokens.push(
+            new Token(
+              TOKENS.Number,
+              number,
+              Number(number),
+              this.line,
+              this.column
+            )
+          )
+        } else if (isChar(char)) {
+          // Identifier or keyword
+          let identifier = [char]
+          while (isAlphanumeric(this.peek())) identifier.push(this.advance())
+          identifier = identifier.join('')
+          if (Object.keys(KEYWORDS).includes(identifier))
+            return this.tokens.push(
+              new Token(
+                TOKENS.Keyword,
+                identifier,
+                KEYWORDS[identifier],
+                this.line,
+                this.column
+              )
+            )
+          else if (identifier === 'true' || identifier === 'false')
+            return this.tokens.push(
+              new Token(TOKENS.Boolean, identifier, identifier === 'true')
+            )
+          return this.tokens.push(
+            new Token(
+              TOKENS.Identifier,
+              identifier,
+              identifier,
+              this.line,
+              this.column
+            )
+          )
+        } else this.error('Unexpected symbol ' + char)
+      }
     }
   }
 
   scanTokens() {
-    while (this.peek() !== '\0') this.scanToken()
-    this.tokens.push(new Token(TOKENS.EOF, null, null, this.line, this.column))
+    while (this.peek() != '\0') this.scanToken()
+    this.tokens.push(new Token(TOKENS.EOF, '\0', '\0', this.line, this.column))
     return this.tokens
-  }
-
-  advance() {
-    if (this.current >= this.program.length) return '\0'
-    this.column++
-    return this.program[this.current++]
-  }
-
-  match(char) {
-    if (this.peek() === char) return this.advance()
-    return false
   }
 }
